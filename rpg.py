83e57b0b9bebad429c0773c4cf4c82ed97c4c687
RafaelPD81 <jogozero@gmail.com>
2025-10-29 21:12:24 -0300

    Adiciona importações

--- rpg.py
+++ rpg.py
@@ -1,1 +1,7 @@
 # Desenvolva o seu jogo aqui
+from itens.item import Item
+from itens.arma import Arma
+from itens.armadura import Armadura
+from personagens.personagem import Personagem
+from personagens.heroi import Heroi
+from personagens.monstro import Monstro

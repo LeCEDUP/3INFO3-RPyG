725260d6489ab17fad13fa88dd162232bc221ce3
Pedro Luca <peluseoni@gmai.com>
2025-11-05 20:54:48 -0300

    feat:✨adiciona_um_map

---
+++ mapa/mapa.py
@@ -0,0 +1,9 @@
+CLASS_MAP = {
+    "Missao": 'Missao',
+    "Consumivel": 'Consumivel',
+    "Equipamento": 'Equipamento',
+    "Luva": 'Luva',
+    "Quimono": 'Quimono',
+    "Acessorio": 'Acessorio',
+    "Habilidade": 'Habilidade',
+}

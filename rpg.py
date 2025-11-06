c1bc95da506cd4e6a65b95321fdad0b70bcb5bcc
bernardo-programmer <ghbernardoprifissional@gmail.com>
2025-11-05 21:05:57 -0300

    modifica armadura(item)

--- rpg.py
+++ rpg.py
@@ -1,22 +1,4 @@
 
-
-
-class Armadura(Item):
-    def __init__(self, nome, descricao, preco, bonus_defesa=0, bonus_resistencia_magica=0):
-        super().__init__(nome, descricao, preco)
-        self.bonus_defesa = bonus_defesa
-        self.bonus_resistencia_magica = bonus_resistencia_magica
-
-    def aplicar_bonus(self, personagem):
-        personagem.defesa += self.bonus_defesa
-        personagem.resistencia_magica += self.bonus_resistencia_magica
-        print(f"{personagem.nome} equipou {self.nome}. Bônus aplicados.")
-
-    def remover_bonus(self, personagem):
-        personagem.defesa -= self.bonus_defesa
-        personagem.resistencia_magica -= self.bonus_resistencia_magica
-
-
 class Magia:
     def __init__(self, nome, tipo_dano, dano_base, custo_mana, descricao):
         self.nome = nome

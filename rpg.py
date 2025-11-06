a9490488b3a139073f8c9729394cec14defd754b
Pedro Luca <peluseoni@gmai.com>
2025-11-05 21:22:50 -0300

    feat:✨ adiciona_import_e_main

--- rpg.py
+++ rpg.py
@@ -1,3 +1,24 @@
-# Desenvolva o seu jogo aqui
+import random
+import time
+import sys
+import json
+import os
+from colorama import Fore, Style, init
 
-#ta dificil veikkkk
+init(autoreset=True)
+
+def main():
+    print(f"{Fore.CYAN}*** RPG DO GUERREIRO DO PUNHO ***{Style.RESET_ALL}")
+    
+    heroi, config_data = load_game()
+    
+    if heroi:
+        print(f"\nBem-vindo de volta, {heroi.nome}!")
+    else:
+        print("\nNenhum jogo salvo encontrado. Iniciando novo jogo.")
+        heroi = criar_novo_heroi()
+        
+    menu_jogo(heroi)
+
+if __name__ == "__main__":
+    main()
